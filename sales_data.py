dc872055d8740ef365dab27e461d93d2a1634dfc
Razvan Burloi <burloi@protonmail.com>
2026-02-28 14:00:53 +0200

    Added total_sales + average_sales functions to sales_data.py

--- sales_data.py
+++ sales_data.py
@@ -2,3 +2,19 @@
 
 sales_data = [230, 200, 310, 290, 400, 150, 180]
 print(f"Sales data for week: {sales_data}.)
+
+def total_sales(data):
+    total = 0
+    for i in data:
+        total += i
+    return total
+    
+def average_sales(data):
+    average = sum(data) / len(data)
+    return average
+    
+result_total_sales = total_sales(sales_data)
+print(f"Total sales is: {result_total_sales}."}
+
+result_average_sales = average_sales(sales_data)
+print(f"Average sales is: {result_total_sales}.")
