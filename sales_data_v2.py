d1ea8d8a0539dd338fc3c1875955abc2bceb24a8
Razvan Burloi <burloi@protonmail.com>
2026-03-03 00:04:42 +0200

    Modified script

---
+++ sales_data_v2.py
@@ -0,0 +1,9 @@
+sales = {
+    "Laptops": 4800,
+    "Smartphones": 6900,
+    "TVs": 9000,
+    "Smartwatches": 8700
+}
+
+total_cost = sum(sales.values())
+print(f"Total products cost in master is: {total_cost}.")
